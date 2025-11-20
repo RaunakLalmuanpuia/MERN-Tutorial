781a86a45fccb7d92f1500d1b2cd265ce2fe8b82
Raunak <rlpuia@outlook.com>
2025-11-20 13:55:44 +0530

    Edit Post

--- frontend/src/pages/Post.js
+++ frontend/src/pages/Post.js
@@ -4,7 +4,7 @@ import {useParams, useNavigate} from "react-router-dom";
 import axios from "axios";
 import {AuthContext} from '../helpers/AuthContext'
 
-function Post(){
+function Post() {
 
     let navigate = useNavigate();
 
@@ -15,29 +15,28 @@ function Post(){
     const [newComment, setNewComment] = useState("");
     const {authState} = useContext(AuthContext);
 
-    useEffect(()=>{
-        axios.get("http://localhost:3001/posts/byId/"+id).then((res) => {
+    useEffect(() => {
+        axios.get("http://localhost:3001/posts/byId/" + id).then((res) => {
             setPostObject(res.data);
         })
 
-        axios.get("http://localhost:3001/comments/"+id).then((res) => {
+        axios.get("http://localhost:3001/comments/" + id).then((res) => {
             setComment(res.data);
         })
-    },[]);
+    }, []);
 
     const addComment = () => {
         axios.post("http://localhost:3001/comments", {
-            commentBody: newComment, PostId:id
-        },{
+            commentBody: newComment, PostId: id
+        }, {
             headers: {
                 accessToken: localStorage.getItem("accessToken"),
             },
         }).then((res) => {
-            if(res.data.error){
+            if (res.data.error) {
                 console.log(res.data.error)
-            }
-            else{
-                const commentToAdd = {commentBody:newComment, username:res.data.username};
+            } else {
+                const commentToAdd = {commentBody: newComment, username: res.data.username};
                 setComment([...comments, commentToAdd]);
                 setNewComment("");
             }
@@ -47,41 +46,83 @@ function Post(){
 
     const deleteComment = (commentId) => {
         console.log(id);
-       axios.delete("http://localhost:3001/comments/"+commentId,{
-           headers:{ accessToken: localStorage.getItem("accessToken"),}
+        axios.delete("http://localhost:3001/comments/" + commentId, {
+            headers: {accessToken: localStorage.getItem("accessToken"),}
         }).then((res) => {
-            if(res.data.error){
+            if (res.data.error) {
                 console.log(res.data.error)
             }
-            setComment(comments.filter((val)=>{
+            setComment(comments.filter((val) => {
                 return val.id !== id;
             }));
-       })
+        })
     }
 
     const deletePost = (id) => {
-        axios.delete("http://localhost:3001/posts/"+id,{
+        axios.delete("http://localhost:3001/posts/" + id, {
             headers: {
                 accessToken: localStorage.getItem("accessToken"),
             }
         }).then((res) => {
-            if(res.data.error){
+            if (res.data.error) {
                 console.log(res.data.error)
             }
             navigate('/');
         })
     }
 
-    return (
-        <div className="postPage">
+    const editPost = (option) => {
+        if (option === 'title') {
+            let newTitle = prompt("Enter New Title");
+            axios.put("http://localhost:3001/posts/title", {
+                newTitle: newTitle,
+                id: id
+            },{
+                headers: {
+                    accessToken: localStorage.getItem("accessToken"),
+                }
+            }).then((res) => {
+                if (res.data.error) {
+                    console.log(res.data.error)
+                }
+                setPostObject({...postObject, title: newTitle});
+            })
+        } else {
+            let newPostText = prompt("Enter New Body");
+            axios.put("http://localhost:3001/posts/postText", {
+                newText: newPostText,
+                id: id
+            },{
+                headers: {
+                    accessToken: localStorage.getItem("accessToken"),
+                }
+            }).then((res) => {
+                if (res.data.error) {
+                    console.log(res.data.error)
+                }
+                setPostObject({...postObject, postText: newPostText});
+            })
+        }
+    }
+
+    return (<div className="postPage">
             <div className="leftSide">
                 <div className="post" id="individual">
-                    <div className="title"> {postObject.title} </div>
-                    <div className="body">{postObject.postText}</div>
+                    <div className="title" onClick={() => {
+                        if(authState.username === postObject.username) {
+                            editPost("title")
+                        }
+                    }}> {postObject.title} </div>
+                    <div className="body" onClick={() => {
+                        if(authState.username === postObject.username) {
+                            editPost("body")
+                        }
+
+                    }}>{postObject.postText}</div>
                     <div className="footer">{postObject.username}
-                        {authState.username === postObject.username &&(
-                            <button onClick={()=> {deletePost(postObject.id)}} >Delete Post</button>
-                        )}
+                        {authState.username === postObject.username && (<button onClick={() => {
+                                deletePost(postObject.id)
+                            }}>Delete Post</button>)}
                     </div>
                 </div>
             </div>
@@ -100,18 +141,16 @@ function Post(){
                 </div>
                 <div className="listOfComments">
                     {comments.map((comment, key) => {
-                        return (
-                            <div key={key} className="comment">
+                        return (<div key={key} className="comment">
                                 {comment.commentBody}
                                 <label>Username: {comment.username}</label>
-                                {authState.username === comment.username && <button onClick={()=>deleteComment(comment.id)}>X</button>}
-                            </div>
-                        );
+                                {authState.username === comment.username &&
+                                    <button onClick={() => deleteComment(comment.id)}>X</button>}
+                            </div>);
                     })}
                 </div>
             </div>
-        </div>
-    );
+        </div>);
 }
 
 export default Post;
